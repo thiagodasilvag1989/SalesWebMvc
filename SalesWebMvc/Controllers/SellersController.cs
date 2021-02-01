7d3072dc792d10b4caf22770adf6ed3d935e1776
Thiago da Silva <thiagodasilvag1989@gmail.com>
2021-02-01 13:12:56 -0300

    Validações

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -39,6 +39,12 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -85,6 +91,7 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult Edit(int? id)
         {
+            
             if (id == null)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not provided" });
@@ -104,7 +111,14 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit (int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                var viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id not mismatch" });
             }
--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -7,17 +7,24 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required (ErrorMessage = "{0} Campo Obrigatório")]
+        [StringLength(60,MinimumLength =3, ErrorMessage ="{0} deve ter o tamanho entre {2} e {1}")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} Campo Obrigatório")]
+        [EmailAddress(ErrorMessage ="Entre com um {0} válido")]
         [Display(Name = "E-Mail")]
         public string Email { get; set; }
 
-        [Display(Name="Data de Nascimento")]
-        [DataType(DataType.Date)]
+        [Required(ErrorMessage = "{0} Campo Obrigatório")]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date)]
+        [Display(Name="Data de Nascimento")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Campo Obrigatório")]
+        [Range(100.0,50000.0, ErrorMessage ="{0} entre {1} e {2}")]
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
